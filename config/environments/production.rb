85d3e27820f592b5d38dc1eacf2ebc8dd5f912bd
今村真人 <imamuramasato@imamuramasato-no-MacBook-Air.local>
2017-06-03 21:25:37 +0900

    device_V11_8

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,8 +80,8 @@ Rails.application.configure do
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
-   user_name: ENV['app66449624@heroku.com'],
-   password: ENV['qwneohfa9678'],
+   user_name: ENV['SENDGRID_USERNAME'],
+   password: ENV['SENDGRID_PASSWORD'],
    domain: "heroku.com",
    address: "smtp.sendgrid.net",
    port: 587,
