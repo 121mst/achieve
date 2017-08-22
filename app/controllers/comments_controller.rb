58a3e0e9d139a71b5f8e4889b31507d1e78a2265
今村真人 <imamuramasato@imamuramasato-no-MacBook-Air.local>
2017-08-22 16:34:53 +0900

    19_2_v2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,6 +18,7 @@ def create
            message: 'あなたの作成したブログにコメントが付きました'
          })
        end
+
        Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'notification_created', {
          unread_counts: Notification.where(user_id: @comment.blog.user.id, read: false).count
        })
@@ -25,6 +26,7 @@ def create
        format.html { render :new }
      end
    end
+
  end
 
 
